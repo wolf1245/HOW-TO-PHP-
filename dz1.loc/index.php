c571164f35e66df7fd9f121dfbc9b5039b1c37e6
Sergey Andreev <wolf22394@gmail.com>
2022-05-24 15:15:43 +0300

    Task1-task2

--- dz1.loc/index.php
+++ dz1.loc/index.php
@@ -10,3 +10,16 @@ function hello() : string
 }
 echo hello();
 echo "<br>";
+/**
+ * Задание 2
+*Создать рекурсивную функцию вычисляющую ряд Фибоначи
+ */
+function fibonacchi($num)
+{
+	if ($num == 0 ) return 0;
+ 
+  if ($num == 1) return 1;
+  return fibonacchi($num - 2) + fibonacchi($num - 1);
+}
+echo fibonacchi(15);
+echo "<br>";
