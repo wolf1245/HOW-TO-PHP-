b8cec048efbd1933f2ace42e600d364782a090d1
Sergey Andreev <wolf22394@gmail.com>
2022-05-24 15:15:29 +0300

    Task1-task1

---
+++ dz1.loc/index.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_type=1);
+/**
+ * Задание 1
+*Создать пользовательскую функцию которая выведет слово "Привет"
+ */
+function hello() : string
+{
+	return "Hello!";
+}
+echo hello();
+echo "<br>";
